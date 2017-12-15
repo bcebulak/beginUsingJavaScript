c0b242768ac210786c5df45761e18e0d8b21be3c
Brandon <bcebulak@msn.com>
2017-12-14 18:54:23 -0700

    4 message

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict"
+//Learner must be able to:
+/*
+//done - create and load a JavaScript file into 
+//- an execution environment such as Node or a browser
+
+//done turn on strict mode and explain why it is 
+//better for scope reasons -- 
+// it is better because it forces variables to be declared 
+//and the browser to use modern ECMA
+
+// done- find their way around proper (e.g. MDN) documentation
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript 
+
+// done - as seen here - comment individual and block comments
+*/ 
+
+//demonstrate how to output results using alert() and console.log() to assist with debugging
+alert("alert worked...")
+console.log("console log worked")
+
+//declare and initialize variables
+let variableDeclared = "a string"; 
+
+//demonstrate understanding of "operators" concept
+let a = 2 + 2
+console.log(a)
+/*
+perform simple tasks using operators and assign the results to variables
+//as above
+*/
+
+
